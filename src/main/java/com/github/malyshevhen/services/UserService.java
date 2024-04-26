1468bdcf60352c2d374b94acd1c3c71e30831d75
Evhen <malysh.evgeniy@gmail.com>
2024-04-26 20:26:01 +0300

    docs: describe user service contract

--- src/main/java/com/github/malyshevhen/services/UserService.java
+++ src/main/java/com/github/malyshevhen/services/UserService.java
@@ -1,5 +1,7 @@
 package com.github.malyshevhen.services;
 
+import com.github.malyshevhen.exceptions.EntityAlreadyExistsException;
+import com.github.malyshevhen.exceptions.EntityNotFoundException;
 import com.github.malyshevhen.models.Address;
 import com.github.malyshevhen.models.User;
 import jakarta.validation.Valid;
@@ -8,21 +10,93 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * Provides methods for managing user data, including creating, retrieving,
+ * updating, and deleting users.
+ *
+ * @author Evhen Malysh
+ */
 @Validated
 public interface UserService {
 
-    User save(@Valid User userToRegister);
+    /**
+     * Saves a new user to the system.
+     *
+     * @param userToRegister the user to be registered
+     * @return the saved user
+     *
+     * @throws {@link EntityAlreadyExistsException} if email provided by the
+     *                registration form is already taken.
+     */
+    User save(@Valid User userToRegister) throws EntityAlreadyExistsException;
 
+    /**
+     * Retrieve page of users based on given pageable.
+     *
+     * @param pageable the pagination details
+     * @return a {@link Page} object containing the list of users
+     *         and page details
+     */
     Page<User> getAll(@NotNull Pageable pageable);
 
-    User getById(@NotNull Long id);
+    /**
+     * Gets the user by identifier.
+     *
+     * @param id The identifier
+     * @return The {@link User} by identifier.
+     *
+     * @throws {@link EntityNotFoundException} if the user for retrieve is not found
+     *                in DB
+     */
+    User getById(@NotNull Long id) throws EntityNotFoundException;
 
-    User updateById(@NotNull Long id, @Valid User user);
+    /**
+     * Update all user fields.
+     *
+     * @param id   The identifier
+     * @param user The parameters for users update
+     * @return Updated {@link User}
+     *
+     * @throws {@link EntityAlreadyExistsException} if the email to update is
+     * @throws {@link EntityNotFoundException} if the user to update is not found in
+     *                DB.
+     */
+    User updateById(@NotNull Long id, @Valid User user)
+            throws EntityNotFoundException, EntityAlreadyExistsException;
 
-    void deleteById(@NotNull Long id);
+    /**
+     * Delete user by unique identifier.
+     *
+     * @param id The identifier
+     *
+     * @throws {@link EntityNotFoundException} if the user is not found
+     */
+    void deleteById(@NotNull Long id) throws EntityNotFoundException;
 
-    User updateEmail(Long id, String email);
+    /**
+     * Update users email field
+     *
+     * @param id    The identifier
+     * @param email The email
+     * @return Updated {@link User}
+     *
+     * @throws {@link EntityAlreadyExistsException} if the email to update is
+     *                already taken.
+     * @throws {@link EntityNotFoundException} if the user for update is not
+     *                found ib DB.
+     */
+    User updateEmail(Long id, String email) throws EntityNotFoundException, EntityAlreadyExistsException;
 
-    User updateAddress(Long id, Address address);
+    /**
+     * Update users address
+     *
+     * @param id      The identifier
+     * @param address The address
+     * @return Updated {@link User}
+     *
+     * @throws {@link EntityNotFoundException} if the user for update is not found
+     *                in DB.
+     */
+    User updateAddress(Long id, Address address) throws EntityNotFoundException;
 
 }

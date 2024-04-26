05b773120865bfbefa1277d8982eaf080018f7c2
Evhen <malysh.evgeniy@gmail.com>
2024-04-26 10:47:11 +0300

    initial commit

---
+++ src/test/java/com/github/malyshevhen/UsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.malyshevhen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

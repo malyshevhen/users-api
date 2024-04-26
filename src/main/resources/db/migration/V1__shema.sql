8aac5103b75bbaeaf772e44f287195ff33c42f6c
Evhen <malysh.evgeniy@gmail.com>
2024-04-26 19:40:50 +0300

    feat: add schema migration

---
+++ src/main/resources/db/migration/V1__shema.sql
@@ -0,0 +1,30 @@
+-- Users
+CREATE TABLE users
+(
+    id         BIGSERIAL    NOT NULL,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    birth_date DATE         NOT NULL,
+    address_id BIGINT,
+    phone      VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (address_id) REFERENCES addresses (id)
+);
+
+-- Addresses
+CREATE TABLE addresses
+(
+    id         BIGSERIAL NOT NULL,
+    country    VARCHAR(255),
+    city       VARCHAR(255),
+    street     VARCHAR(255),
+    number     VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+
+    PRIMARY KEY (id)
+);

09bdf53b0387679e46198389b0ac259f797dc695
Evhen <malysh.evgeniy@gmail.com>
2024-04-27 23:06:55 +0300

    docs: add to user constrains contract

--- src/main/java/com/github/malyshevhen/configs/UserConstraints.java
+++ src/main/java/com/github/malyshevhen/configs/UserConstraints.java
@@ -1,5 +1,10 @@
 package com.github.malyshevhen.configs;
 
+/**
+ * Defines the required age constraint for users.
+ * 
+ * @author Evhen Malysh
+ */
 public interface UserConstraints {
     int getRequiredAge();
 }
